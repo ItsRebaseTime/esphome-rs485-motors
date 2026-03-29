42667c3cca5be458ad6ba54f3c8089de5459bf23
ItsRebaseTime <itsrebasetime@protonmail.com>
2026-03-29 10:34:47 +0300

    Fix invert direction and pull to start switches

--- esphome/components/dooya/switch/dooya_switch.cpp
+++ esphome/components/dooya/switch/dooya_switch.cpp
@@ -9,7 +9,7 @@ void InvertDirectionSwitch::write_state(bool state) {
   ESP_LOGI(TAG, "%s invert direction", state ? "Enabling" : "Disabling");
   uint8_t payload = state ? 0x01 : 0x00;
   uint8_t data[4] = {WRITE, INVERT_DIRECTION, 0x01, payload};
-  this->parent_->send_command(data, 2);
+  this->parent_->send_command(data, 4);
   this->parent_->current_write_payload = payload;
 }
 
@@ -17,7 +17,7 @@ void PullToStartSwitch::write_state(bool state) {
   ESP_LOGI(TAG, "%s pull to start", state ? "Enabling" : "Disabling");
   uint8_t payload = state ? 0x00 : 0x01;
   uint8_t data[4] = {WRITE, PULL_TO_START, 0x01, payload};
-  this->parent_->send_command(data, 2);
+  this->parent_->send_command(data, 4);
   this->parent_->current_write_payload = payload;
 }
 
